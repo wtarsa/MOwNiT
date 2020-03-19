d4989d616a4330a403bcc29486326706f31453de
Wiktor Tarsa <wiktor.tarsa@gmail.com>
2020-03-19 18:59:09 +0100

    first steps wit lab2

---
+++ lab2/zadanie1/aghMatrix.cpp
@@ -0,0 +1,139 @@
+#include "aghMatrix.h"
+
+
+// Parameter Constructor                                                                                                                                                      
+template<typename T>
+AGHMatrix<T>::AGHMatrix(const std::vector<std::vector<T>>& mat) 
+{
+  matrix.resize(mat.size());
+  for (unsigned i = 0; i < mat.size(); i++) 
+  {
+    matrix[i].resize(mat[i].size());
+    for(unsigned j = 0; j < mat[i].size(); j++)
+    {
+      matrix[i][j] = mat[i][j];
+    }
+  }
+  rows = matrix.size();
+  cols = matrix[1].size();
+}
+
+// Parameter Constructor                                                                                                                                                      
+template<typename T>
+AGHMatrix<T>::AGHMatrix(unsigned _rows, unsigned _cols, const T& _initial) 
+{
+  matrix.resize(_rows);
+  for (unsigned i=0; i<matrix.size(); i++) 
+  {
+    matrix[i].resize(_cols, _initial);
+  }
+  rows = _rows;
+  cols = _cols;
+}
+
+// Copy Constructor                                                                                                                                                           
+template<typename T>
+AGHMatrix<T>::AGHMatrix(const AGHMatrix<T>& rhs) 
+{
+  matrix = rhs.matrix;
+  rows = rhs.get_rows();
+  cols = rhs.get_cols();
+}
+
+// Get the number of rows of the matrix                                                                                                                                       
+template<typename T>
+unsigned AGHMatrix<T>::get_rows() const 
+{
+  return this->rows;
+}
+
+// Get the number of columns of the matrix                                                                                                                                    
+template<typename T>
+unsigned AGHMatrix<T>::get_cols() const 
+{
+  return this->cols;
+}
+
+// Assignment Operator                                                                                                                                                        
+template<typename T>
+AGHMatrix<T>& AGHMatrix<T>::operator=(const AGHMatrix<T>& rhs) 
+{
+  if (&rhs == this)
+    return *this;
+
+  unsigned new_rows = rhs.get_rows();
+  unsigned new_cols = rhs.get_cols();
+
+  matrix.resize(new_rows);
+  for (unsigned i=0; i<matrix.size(); i++) 
+  {
+    matrix[i].resize(new_cols);
+  }
+
+  for (unsigned i=0; i<new_rows; i++) 
+  {
+    for (unsigned j=0; j<new_cols; j++) 
+    {
+      matrix[i][j] = rhs(i, j);
+    }
+  }
+  rows = new_rows;
+  cols = new_cols;
+
+  return *this;
+}
+
+// Access the individual elements                                                                                                                                             
+template<typename T>
+T& AGHMatrix<T>::operator()(const unsigned& row, const unsigned& col) 
+{
+  return this->matrix[row][col];
+}
+
+// Access the individual elements (const)                                                                                                                                     
+template<typename T>
+const T& AGHMatrix<T>::operator()(const unsigned& row, const unsigned& col) const 
+{
+  return this->matrix[row][col];
+}
+
+// Addition of two matrices                                                                                                                                                   
+template<typename T>
+AGHMatrix<T> AGHMatrix<T>::operator+(const AGHMatrix<T>& rhs) 
+{
+    AGHMatrix<T> result(rhs);
+    for(int r = 0; r < rows; r++){
+        for(int c = 0; c < cols; c++){
+            result.matrix[r][c] += matrix[r][c];
+        }
+    }
+    return result;
+}
+
+// Left multiplication of this matrix and another                                                                                                                              
+template<typename T>
+AGHMatrix<T> AGHMatrix<T>::operator*(const AGHMatrix<T>& rhs) 
+{
+    AGHMatrix<T> result(matrix);
+    for(int r = 0; r < rows; r++){
+        for(int c = 0; c < rhs.cols; c++){
+            result.matrix[r][c] += matrix[r][c];
+        }
+    }
+    return result;
+}
+
+// Printing matrix                                                                                                                        
+template<typename T>
+std::ostream& operator<<(std::ostream& stream, const AGHMatrix<T>& matrix) 
+{
+  for (int i=0; i<matrix.get_rows(); i++) 
+  { 
+    for (int j=0; j<matrix.get_cols(); j++) 
+    {
+        stream << matrix(i,j) << ", ";
+    }
+    stream << std::endl;
+  }
+    stream << std::endl;
+}
